b467c3289f719cd01a84fac97a39335f7e82cbf0
hritiksah <ithritik64@gmail.com>
2024-04-14 12:18:42 +0545

    Week 1 comment

--- week1/Hello.java
+++ week1/Hello.java
@@ -1,9 +1,10 @@
 package week1;
+/*1. Write a program that prints Hello World! to the console. Save the program as Hello.java */
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+       
     }
     
-
 }
