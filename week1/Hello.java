ff5956b07be2d5737cc48c7d6c7dafe23f24ee54
hritiksah <ithritik64@gmail.com>
2024-04-15 10:12:01 +0545

    change comment

--- week1/Hello.java
+++ week1/Hello.java
@@ -36,7 +36,7 @@ System.out.print (not println) and produces the same output */
 // What is the difference between
 // a ' and a "?  Or between a " and a \"?
 // One is what we see when we're typing our program. The other is what appears on the "console."
-        System.out.println("A well-formed Java program\nhas a main method with {\nand } braces.\nA System.out.println\nstatement has ( and ) and\nand ends with a \" character.\n(But we type \" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"?\nOne is what we see when we're typing our\nprogram. The other is what appears on the\n\"console.\"");
+        System.out.println("A well-formed Java program\nhas a main method with {\nand } braces.\n\nA System.out.println\nstatement has ( and ) and\nand ends with a \" character.\n(But we type \" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \"?\n\nOne is what we see when we're typing our\nprogram. The other is what appears on the\n\"console.\"");
 
     }
     
